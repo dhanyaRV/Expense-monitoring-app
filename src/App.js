f28f0933412881e4dd6e9ee47812b00a6b958fc0
Dhanya Paulose <dhanyapaulose20@gmail.com>
2021-07-26 10:21:12 +0530

    events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
 
+import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -23,31 +25,26 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense =>{
+    console.log(' In App.js');
+    console.log(expense)
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-      title={expenses[0].title}
-      amount={expenses[0].amount}
-      date ={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-       title={expenses[1].title}
-       amount={expenses[1].amount}
-       date ={expenses[1].date}
-       ></ExpenseItem>
-      <ExpenseItem 
-       title={expenses[2].title}
-       amount={expenses[2].amount}
-       date ={expenses[2].date}
-       ></ExpenseItem>
-      <ExpenseItem
-       title={expenses[3].title}
-       amount={expenses[3].amount}
-       date ={expenses[3].date}
-       ></ExpenseItem>
+      
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
     </div>
   );
-}
 
+  // return React.createElement(
+  //   'div', //html element
+  //   {},//attributes or properties or objects
+  //                                 //contents
+  //   React.createElement('h2',{},"Let's get started!"),
+  //   React.createElement(Expenses,{items:expenses})
+  //   );
+  }
 export default App;
